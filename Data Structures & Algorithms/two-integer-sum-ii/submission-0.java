0a76de8580fd6058dc89ba4da23f971d06c09cbb
Aditya Thakur <149915605+AdiHustler@users.noreply.github.com>
2026-06-22 22:10:43 +0530

    Fix while loop condition in twoSum method

--- Data Structures & Algorithms/two-integer-sum-ii/submission-0.java
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.java
@@ -3,8 +3,7 @@ class Solution {
         int[] ans = new int[2];
         int start = 0;
         int end = numbers.length-1;
-        while(start<=end){
-            int mid = start + ((end-start)/2);
+        while(start<end){
             if(numbers[start]+ numbers[end] == target){
                 ans[0]  = start+1;
                 ans[1]  = end+1;
